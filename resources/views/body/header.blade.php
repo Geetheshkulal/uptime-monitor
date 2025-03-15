647af06f34ccc8502571ad80676d2a2595b37e96
Ananth-Joshi <ananthj82@gmail.com>
2025-03-15 13:06:51 +0530

    added profile pic functionality

--- resources/views/body/header.blade.php
+++ resources/views/body/header.blade.php
@@ -1,6 +1,3 @@
-
-
-
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
     <!-- Sidebar Toggle (Topbar) -->
@@ -172,9 +169,9 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
                 <img class="img-profile rounded-circle"
-                    src="img/undraw_profile.svg">
+                    src="{{ Avatar::create(auth()->user()->name)->toBase64() }}">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
