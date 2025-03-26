0924a008bbfeff43b98963edf2b43d86b4456784
Ananth-Joshi <ananthj82@gmail.com>
2025-03-26 10:22:03 +0530

    fixes

--- resources/views/body/header.blade.php
+++ resources/views/body/header.blade.php
@@ -145,11 +145,8 @@ body.dark .dropdown-item:hover {
         </div>
     </form>
 
-<<<<<<< HEAD
-=======
  
 
->>>>>>> 2ca890fb00a6c1827517289b927948cf3f86a424
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
 
@@ -177,8 +174,6 @@ body.dark .dropdown-item:hover {
             </div>
         </li>
 
-<<<<<<< HEAD
-=======
         {{-- dark mode and light mode --}}
 
         {{-- <div>
@@ -195,20 +190,15 @@ body.dark .dropdown-item:hover {
                  $notifications = auth()->user()->unreadNotifications;
                  $notificationCount = $notifications->count();
           @endphp
->>>>>>> 2ca890fb00a6c1827517289b927948cf3f86a424
         <!-- Nav Item - Alerts -->
         <li class="nav-item dropdown no-arrow mx-1">
             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-bell fa-fw"></i>
                 <!-- Counter - Alerts -->
-<<<<<<< HEAD
-                <span class="badge badge-danger badge-counter">3+</span>
-=======
                 @if($notificationCount > 0)
                 <span class="badge badge-danger badge-counter">{{ $notificationCount }}</span>
             @endif
->>>>>>> 2ca890fb00a6c1827517289b927948cf3f86a424
             </a>
             <!-- Dropdown - Alerts -->
             <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -216,41 +206,6 @@ body.dark .dropdown-item:hover {
                 <h6 class="dropdown-header">
                     Alerts Center
                 </h6>
-<<<<<<< HEAD
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="mr-3">
-                        <div class="icon-circle bg-primary">
-                            <i class="fas fa-file-alt text-white"></i>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="small text-gray-500">December 12, 2019</div>
-                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="mr-3">
-                        <div class="icon-circle bg-success">
-                            <i class="fas fa-donate text-white"></i>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="small text-gray-500">December 7, 2019</div>
-                        $290.29 has been deposited into your account!
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="mr-3">
-                        <div class="icon-circle bg-warning">
-                            <i class="fas fa-exclamation-triangle text-white"></i>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="small text-gray-500">December 2, 2019</div>
-                        Spending Alert: We've noticed unusually high spending for your account.
-                    </div>
-                </a>
-=======
                 
                 @forelse($notifications as $notification)
                 <a class="dropdown-item d-flex align-items-center" href="{{ $notification->data['url'] }}">
@@ -263,80 +218,11 @@ body.dark .dropdown-item:hover {
             @endforelse
             <a class="dropdown-item text-center small text-gray-500" href="{{ route('notifications.read') }}">Mark all as read</a>
 
->>>>>>> 2ca890fb00a6c1827517289b927948cf3f86a424
                 <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
             </div>
         </li>
 
-<<<<<<< HEAD
-        <!-- Nav Item - Messages -->
-        <li class="nav-item dropdown no-arrow mx-1">
-            <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-envelope fa-fw"></i>
-                <!-- Counter - Messages -->
-                <span class="badge badge-danger badge-counter">7</span>
-            </a>
-            <!-- Dropdown - Messages -->
-            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                aria-labelledby="messagesDropdown">
-                <h6 class="dropdown-header">
-                    Message Center
-                </h6>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="img/undraw_profile_1.svg"
-                            alt="...">
-                        <div class="status-indicator bg-success"></div>
-                    </div>
-                    <div class="font-weight-bold">
-                        <div class="text-truncate">Hi there! I am wondering if you can help me with a
-                            problem I've been having.</div>
-                        <div class="small text-gray-500">Emily Fowler · 58m</div>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="img/undraw_profile_2.svg"
-                            alt="...">
-                        <div class="status-indicator"></div>
-                    </div>
-                    <div>
-                        <div class="text-truncate">I have the photos that you ordered last month, how
-                            would you like them sent to you?</div>
-                        <div class="small text-gray-500">Jae Chun · 1d</div>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="img/undraw_profile_3.svg"
-                            alt="...">
-                        <div class="status-indicator bg-warning"></div>
-                    </div>
-                    <div>
-                        <div class="text-truncate">Last month's report looks great, I am very happy with
-                            the progress so far, keep up the good work!</div>
-                        <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                    </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"
-                            alt="...">
-                        <div class="status-indicator bg-success"></div>
-                    </div>
-                    <div>
-                        <div class="text-truncate">Am I a good boy? The reason I ask is because someone
-                            told me that people say this to all dogs, even if they aren't good...</div>
-                        <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                    </div>
-                </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
-            </div>
-        </li>
-=======
     
->>>>>>> 2ca890fb00a6c1827517289b927948cf3f86a424
 
         <div class="topbar-divider d-none d-sm-block"></div>
 
@@ -373,9 +259,6 @@ body.dark .dropdown-item:hover {
 
     </ul>
 
-<<<<<<< HEAD
-</nav>
-=======
 </nav>
 
 <script>
@@ -397,4 +280,3 @@ checkbox.addEventListener("change", () => {
     }
 });
 </script>
->>>>>>> 2ca890fb00a6c1827517289b927948cf3f86a424
