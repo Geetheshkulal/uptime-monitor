c926b2a16f5280009947a235499b735fa79ff00b
geethesh kulal <geetheshkulal84@gmail.com>
2025-04-04 16:21:37 +0530

    animation

--- resources/views/body/sidebar.blade.php
+++ resources/views/body/sidebar.blade.php
@@ -1,4 +1,7 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+
+
+<ul data-aos="fade-right" class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
