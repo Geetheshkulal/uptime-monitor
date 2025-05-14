b18ae67d4140135d26b232f2a413bf8756ea87ad
Shalmali <shalmalikava@gmail.com>
2025-05-14 09:48:01 +0530

    fixed ham

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -152,9 +152,8 @@
     <!-- JS Scripts -->
     <script src="{{ asset('frontend/assets/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    
-    <script src="{{ asset('frontend/assets/js/sb-admin-2.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/demo/chart-area-demo.js') }}"></script>
+    <script src="js/sb-admin-2.min.js"></script>
+    <script src="js/demo/chart-area-demo.js"></script>
     <script src="{{ asset('frontend/assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
     <script src="{{ asset('frontend/assets/vendor/chart.js/Chart.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
