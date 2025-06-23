cadb20602f7a988fb73cec723be6e27873bcb659
Ananth-Joshi <ananthj82@gmail.com>
2025-06-23 11:45:07 +0530

    conflict merged

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -55,39 +55,4 @@ abstract class DuskTestCase extends BaseTestCase
 }
 
 
-    /**
-     * Create the RemoteWebDriver instance.
-     */
-    // protected function driver(): RemoteWebDriver
-    // {
-    //     $userDataDir = storage_path('whatsapp-session');
-
-    //     $options = (new ChromeOptions)->addArguments(collect([
-    //         $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
-    //         '--disable-search-engine-choice-screen',
-    //         '--disable-smooth-scrolling',
-    //         '--user-data-dir=' . $userDataDir, // Persist session
-    //         '--profile-directory=Default', 
-
-    //     ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
-    //         return $items->merge([
-    //             '--disable-gpu',
-    //             //'--headless=new',
-    //         ]);
-    //     })->all());
-
-    //     return RemoteWebDriver::create( 
-    //         $_ENV['DUSK_DRIVER_URL'] ?? env('DUSK_DRIVER_URL') ?? 'http://localhost:9515',
-    //         DesiredCapabilities::chrome()->setCapability(
-    //             ChromeOptions::CAPABILITY, $options
-    //         )
-    //     );
-    // }
-
-
-
-
-
-
-
-
+  
